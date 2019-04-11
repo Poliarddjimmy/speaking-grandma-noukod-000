cddf9f6b6df1554ac8d6227eaf1bf2da9c94d5a6
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-11 15:07:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!" 
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?" || 
+  elsif phrase == "WHAT DID YOU EAT TODAY?" || phrase == "WHAT?"
     return "NO, NOT SINCE 1938!"
   end
 end
